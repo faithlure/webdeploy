62d634b545e57cb30b927678a0b95b588bfdc115
Aditya Prayoga <adityaprayoga32@gmail.com>
2024-05-10 11:16:18 +0700

    benerin database.js

--- config/Database.js
+++ config/Database.js
@@ -2,7 +2,8 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize('crud_db', 'root','migrasidata123',{
     host: '34.41.198.39',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    
 });
 
 export default db;
